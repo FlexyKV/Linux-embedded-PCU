16bc8ea703aa7e465f5790f7e327968f794a8b29
Philippe Rochette Laplante <philippe.rochette-laplante.1@ulaval.ca>
2022-02-14 17:13:31 -0500

    Integration Database/logging

--- software/pcu/src/Log/loggingSyslog.py
+++ software/pcu/src/Log/loggingSyslog.py
@@ -29,23 +29,13 @@ class loggingSyslog(object):
 
     def logging_valeurs(self):
         all_avg_mesures = self.get_avg_mesure()
-        print("Port:" , all_avg_mesures[0][0], "Currents:", all_avg_mesures[0][1], "Power:", all_avg_mesures[0][2])
-        print("Port:", all_avg_mesures[1][0], "Currents:", all_avg_mesures[1][1], "Power:", all_avg_mesures[1][2])
-        print("Port:", all_avg_mesures[2][0], "Currents:", all_avg_mesures[2][1], "Power:", all_avg_mesures[2][2])
-        print("Port:", all_avg_mesures[3][0], "Currents:", all_avg_mesures[3][1], "Power:", all_avg_mesures[3][2])
-        print("Port:", all_avg_mesures[4][0], "Currents:", all_avg_mesures[4][1], "Power:", all_avg_mesures[4][2])
-        print("Port:", all_avg_mesures[5][0], "Currents:", all_avg_mesures[5][1], "Power:", all_avg_mesures[5][2])
-        print("Port:", all_avg_mesures[6][0], "Currents:", all_avg_mesures[6][1], "Power:", all_avg_mesures[6][2])
-        print("Port:", all_avg_mesures[7][0], "Currents:", all_avg_mesures[7][1], "Power:", all_avg_mesures[7][2])
+        for i in range(8):
+            #logging.info("Port:" , all_avg_mesures[i][0], "Power avg:", (all_avg_mesures[i][1].pop() * all_avg_mesures[i][2].pop()) )
+            print("Port:", all_avg_mesures[i][0],
+                         "Power avg:",(all_avg_mesures[i][1].pop() * all_avg_mesures[i][2].pop()),"W")
 
-        #logging.info('Puissance moyenne: %s  Courant:%s ', a, b)
-
-
-        #a = random.randrange(10)
-        #b = random.randrange(50)
         #threading.Timer(10.0, self.logging_valeurs).start()
-        #logging.info('Puissance moyenne: %s  Courant:%s ', a, b)
-        #logging.info('Puissance moyenne: %s  Courant:%s  ADC0: %s  ADC1: %s', a, b,c,d)
+
 
     def get_avg_mesure(self):
         end_time = datetime.now()
@@ -53,10 +43,15 @@ class loggingSyslog(object):
         port_avg_data = []
         for i in range(8):
             port_data = self.repository.get_port_measures(i, start_time, end_time)
-            mapper = MeasureMapper(*port_data, 3600, start_time, end_time)
-            mapped_port_data = mapper.map_measures()
-            port_avg_data.append((i, mapped_port_data[1], mapped_port_data[2]))
-        print(port_avg_data)
+            #If no data
+            if port_data == -1:
+                logging.info("Aucune valeur pour la dernière heure")
+                return 0
+            else:
+                mapper = MeasureMapper(*port_data, 3600, start_time, end_time)
+                mapped_port_data = mapper.map_measures()
+                port_avg_data.append((i, mapped_port_data[1], mapped_port_data[2]))
+        #print(port_avg_data)
         return port_avg_data
 
 
